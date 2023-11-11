f5b915240a365724d9951104d3a006e2f18f9e1a
Yadvendra Singh <yadvendra187@gmail.com>
2023-11-11 17:06:48 +0530

    Final Commit

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ from fpdf import FPDF
 
 filepaths = glob.glob("Files/*.txt")
 pdf = FPDF(orientation="P", format="A4", unit="mm")
+
 for filepath in filepaths:
     titles = Path(filepath).stem.title()
+    with open(filepath,"r") as file:
+        text = file.read()
     pdf.add_page()
     pdf.set_font(family="Arial", style="B", size=20)
     pdf.cell(txt=titles, w=20, h=10, ln=1)
-
+    pdf.set_font(family="Arial", style="", size=10)
+    pdf.multi_cell(txt=text, w=0, h=10)
 pdf.output('output.pdf')
